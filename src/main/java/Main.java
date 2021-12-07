cceaa5d0fdb92dfa17b6452df332ef6f8628b57a
Kamil Mosek <kamil.mosek@gmail.com>
2021-12-07 15:31:25 +0100

    connect with apache jena to dbpedia add execute example query

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,24 @@
+import lombok.extern.slf4j.Slf4j;
+import org.apache.jena.query.*;
+import org.apache.jena.sparql.engine.http.QueryEngineHTTP;
+
 public class Main {
     public static void main(String[] args) {
+        String queryStr = "SELECT ?prop ?place WHERE { <http://dbpedia.org/resource/Paris> ?prop ?place .}";
+        Query query = QueryFactory.create(queryStr);
 
+        // Remote execution.
+        try {
+            QueryExecution qexec = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", query);
+            // Set the DBpedia specific timeout.
+            ((QueryEngineHTTP)qexec).addParam("timeout", "10000") ;
+
+            // Execute.
+            ResultSet rs = qexec.execSelect();
+            ResultSetFormatter.out(System.out, rs, query);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
+
