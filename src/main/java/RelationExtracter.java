13899a8e2d37c1347be422cd66a43392fad7a934
Kamil Mosek <kamil.mosek@gmail.com>
2021-12-29 18:46:29 +0100

    add edu.stanford dependency to pom

---
+++ src/main/java/RelationExtracter.java
@@ -0,0 +1,96 @@
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.CoreDocument;
+import edu.stanford.nlp.pipeline.CoreSentence;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StringTransformation {
+    private final String text;
+    private CoreDocument coreDocument;
+    private StanfordCoreNLP stanfordCoreNLP;
+    private List<CoreLabel> coreLabelList;
+    private List<CoreSentence> sentenceList;
+    File file;// = new File("filename.txt");
+    private final String filename;
+
+    public StringTransformation(String text) {
+        this.text = text;
+        this.stanfordCoreNLP = Pipeline.getPipeline();
+        this.coreDocument = new CoreDocument(text);
+        stanfordCoreNLP.annotate(coreDocument);
+        tokenizeText();
+        breakTextIntoSentences();
+        this.filename = UUID.randomUUID().toString() + ".txt";
+        createFile();
+    }
+
+    public void getRelations() throws IOException {
+        FileWriter fileWriter = new FileWriter(this.filename);
+
+        System.out.println("Tokenizacja");
+        fileWriter.write("ORIGINAL TEXT: \n");
+        fileWriter.write(this.text + "\n");
+        for (CoreLabel c : coreLabelList) {
+            System.out.println(c.originalText());
+        }
+
+        System.out.println("DIVISION OF INPUT INTO SENTENCES AND THEIR SENTIMENT");
+        fileWriter.write("\n\nDIVISION OF INPUT INTO SENTENCES AND THEIR SENTIMENT \t\t\n");
+        List<CoreSentence> sentenceList = coreDocument.sentences();
+        for (CoreSentence s : sentenceList) {
+            System.out.println("Sentiment: " + s.sentiment() + "\t" + s.toString());
+            fileWriter.write("Sentiment: " + s.sentiment() + "\t" + s.toString() + "\n");
+        }
+
+
+        System.out.println("\n\nRELATION\n\nPOSITION | LEMMAEMMATIZATION | NAMED ENTITY RECOGNITION\n");
+        fileWriter.write("\n\nRELATION\n\nPOSITION | LEMMAEMMATIZATION | NAMED ENTITY RECOGNITION\n");
+        for (CoreLabel coreLabel : coreLabelList) {
+
+            String position = coreLabel.get(CoreAnnotations.PartOfSpeechAnnotation.class);
+            String lemma = coreLabel.lemma();
+            String ner = coreLabel.get(CoreAnnotations.NamedEntityTagAnnotation.class);
+            System.out.println(coreLabel.originalText() + "\t\t\t position: " + position +
+                    "\t\t\t lemmaemmatization: " + lemma + "\t\t\t named entity recognition: " + ner);
+            fileWriter.write("original text: " + coreLabel.originalText() + "\t\t\t position: " + position +
+                    "\t\t\t lemmaemmatization: " + lemma + "\t\t\t named entity recognition: " + ner + "\n");
+
+        }
+
+        System.out.println("RESULTS SAVED TO FILE " + this.filename);
+        fileWriter.close();
+    }
+
+    private void tokenizeText() {
+        this.coreLabelList = coreDocument.tokens();
+    }
+
+    private void breakTextIntoSentences() {
+        this.sentenceList = coreDocument.sentences();
+    }
+
+    private void createFile() {
+        try {
+            this.file = new File(this.filename);
+            if (this.file.createNewFile()) {
+                System.out.println("File created: " + this.file.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
